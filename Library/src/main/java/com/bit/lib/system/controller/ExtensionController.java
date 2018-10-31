6ce7af7cb8c917c46ccdf5a756ea69325da39c73
JongChan-Park <parkgkgk@naver.com>
2018-10-31 10:47:19 +0900

    2018-10-31 11:00 Jchan

--- Library/src/main/java/com/bit/lib/system/controller/ExtensionController.java
+++ Library/src/main/java/com/bit/lib/system/controller/ExtensionController.java
@@ -2,12 +2,16 @@ package com.bit.lib.system.controller;
 
 import com.bit.lib.system.model.MybookList;
 import com.bit.lib.system.service.ExtensionService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ExtensionController {
@@ -27,9 +31,17 @@ public class ExtensionController {
     }
 
 @RequestMapping(value = "admin/borrow/extension", method=RequestMethod.GET)
-    public String approved(){
+    public String approved(Model model){
+
+       List<MybookList> list = service.getList();
+       model.addAttribute("list",list);
+        return "admin/borrow/extensionlist";
+    }
 
 
-        return "";
+    @RequestMapping(value = "admin/borrow/extension", method=RequestMethod.POST)
+    public String updateapproved(@RequestParam("book_code") String code){
+        service.extensionOk(code);
+        return "redirect:/admin/borrow/extension";
     }
 }
